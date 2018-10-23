b7477d45c812bddf77c9021cdae67291f78f7fff
githamm <khamm@denverpost.com>
2018-10-23 17:21:53 -0600

    Non d3 svg with test data working -- tooltips, not colors yet

---
+++ map/test-data.js
@@ -0,0 +1,834 @@
+[{
+    "08001": [{
+        "candidate": "Jared Polis",
+        "votes": "106415"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "94738"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "6578"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "6513"
+    }],
+    "08003": [{
+        "candidate": "Jared Polis",
+        "votes": "3901"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "3357"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "261"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "280"
+    }],
+    "08005": [{
+        "candidate": "Jared Polis",
+        "votes": "165066"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "143820"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "8707"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "7310"
+    }],
+    "08007": [{
+        "candidate": "Jared Polis",
+        "votes": "3244"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "4583"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "250"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "239"
+    }],
+    "08009": [{
+        "candidate": "Jared Polis",
+        "votes": "461"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "1586"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "74"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "69"
+    }],
+    "08011": [{
+        "candidate": "Jared Polis",
+        "votes": "805"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "1400"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "55"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "75"
+    }],
+    "08013": [{
+        "candidate": "Jared Polis",
+        "votes": "126460"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "47665"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "5730"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "6775"
+    }],
+    "08014": [{
+        "candidate": "Jared Polis",
+        "votes": "19687"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "17284"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "1138"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "970"
+    }],
+    "08015": [{
+        "candidate": "Jared Polis",
+        "votes": "5383"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "5440"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "363"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "371"
+    }],
+    "08017": [{
+        "candidate": "Jared Polis",
+        "votes": "170"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "860"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "31"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "32"
+    }],
+    "08019": [{
+        "candidate": "Jared Polis",
+        "votes": "3261"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "2563"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "197"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "225"
+    }],
+    "08021": [{
+        "candidate": "Jared Polis",
+        "votes": "2044"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "1990"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "91"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "94"
+    }],
+    "08023": [{
+        "candidate": "Jared Polis",
+        "votes": "1429"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "616"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "66"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "74"
+    }],
+    "08025": [{
+        "candidate": "Jared Polis",
+        "votes": "424"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "1114"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "67"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "66"
+    }],
+    "08027": [{
+        "candidate": "Jared Polis",
+        "votes": "931"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "2024"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "82"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "71"
+    }],
+    "08029": [{
+        "candidate": "Jared Polis",
+        "votes": "4921"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "10898"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "546"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "575"
+    }],
+    "08031": [{
+        "candidate": "Jared Polis",
+        "votes": "263875"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "77284"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "7657"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "8981"
+    }],
+    "08033": [{
+        "candidate": "Jared Polis",
+        "votes": "371"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "861"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "44"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "41"
+    }],
+    "08035": [{
+        "candidate": "Jared Polis",
+        "votes": "69289"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "111243"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "4402"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "3967"
+    }],
+    "08037": [{
+        "candidate": "Jared Polis",
+        "votes": "16267"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "10110"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "556"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "547"
+    }],
+    "08039": [{
+        "candidate": "Jared Polis",
+        "votes": "3382"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "11424"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "504"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "420"
+    }],
+    "08041": [{
+        "candidate": "Jared Polis",
+        "votes": "116526"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "215882"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "12628"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "9684"
+    }],
+    "08043": [{
+        "candidate": "Jared Polis",
+        "votes": "6826"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "15693"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "619"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "728"
+    }],
+    "08045": [{
+        "candidate": "Jared Polis",
+        "votes": "12801"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "13766"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "892"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "771"
+    }],
+    "08047": [{
+        "candidate": "Jared Polis",
+        "votes": "1947"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "1616"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "170"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "167"
+    }],
+    "08049": [{
+        "candidate": "Jared Polis",
+        "votes": "4072"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "4691"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "283"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "215"
+    }],
+    "08051": [{
+        "candidate": "Jared Polis",
+        "votes": "5884"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "3789"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "305"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "331"
+    }],
+    "08053": [{
+        "candidate": "Jared Polis",
+        "votes": "234"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "334"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "16"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "16"
+    }],
+    "08055": [{
+        "candidate": "Jared Polis",
+        "votes": "2010"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "1861"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "108"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "102"
+    }],
+    "08057": [{
+        "candidate": "Jared Polis",
+        "votes": "200"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "718"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "16"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "17"
+    }],
+    "08059": [{
+        "candidate": "Jared Polis",
+        "votes": "166863"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "147432"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "8618"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "9816"
+    }],
+    "08061": [{
+        "candidate": "Jared Polis",
+        "votes": "114"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "639"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "23"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "25"
+    }],
+    "08063": [{
+        "candidate": "Jared Polis",
+        "votes": "792"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "2857"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "108"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "92"
+    }],
+    "08065": [{
+        "candidate": "Jared Polis",
+        "votes": "2248"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "1453"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "129"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "139"
+    }],
+    "08067": [{
+        "candidate": "Jared Polis",
+        "votes": "17979"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "14389"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "1013"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "1068"
+    }],
+    "08069": [{
+        "candidate": "Jared Polis",
+        "votes": "96603"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "89560"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "5382"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "6311"
+    }],
+    "08071": [{
+        "candidate": "Jared Polis",
+        "votes": "3727"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "3939"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "291"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "255"
+    }],
+    "08073": [{
+        "candidate": "Jared Polis",
+        "votes": "497"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "1854"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "69"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "61"
+    }],
+    "08075": [{
+        "candidate": "Jared Polis",
+        "votes": "2626"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "6922"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "213"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "220"
+    }],
+    "08077": [{
+        "candidate": "Jared Polis",
+        "votes": "31129"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "54533"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "2261"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "2860"
+    }],
+    "08079": [{
+        "candidate": "Jared Polis",
+        "votes": "293"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "310"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "20"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "19"
+    }],
+    "08081": [{
+        "candidate": "Jared Polis",
+        "votes": "1282"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "5953"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "254"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "199"
+    }],
+    "08083": [{
+        "candidate": "Jared Polis",
+        "votes": "5321"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "9131"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "416"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "361"
+    }],
+    "08085": [{
+        "candidate": "Jared Polis",
+        "votes": "6433"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "13946"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "629"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "515"
+    }],
+    "08087": [{
+        "candidate": "Jared Polis",
+        "votes": "3618"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "8356"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "338"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "334"
+    }],
+    "08089": [{
+        "candidate": "Jared Polis",
+        "votes": "3639"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "5313"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "272"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "287"
+    }],
+    "08091": [{
+        "candidate": "Jared Polis",
+        "votes": "1863"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "1467"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "89"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "85"
+    }],
+    "08093": [{
+        "candidate": "Jared Polis",
+        "votes": "4051"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "6490"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "420"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "358"
+    }],
+    "08095": [{
+        "candidate": "Jared Polis",
+        "votes": "589"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "1804"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "64"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "51"
+    }],
+    "08097": [{
+        "candidate": "Jared Polis",
+        "votes": "8325"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "2803"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "336"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "298"
+    }],
+    "08099": [{
+        "candidate": "Jared Polis",
+        "votes": "1473"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "3856"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "195"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "170"
+    }],
+    "08101": [{
+        "candidate": "Jared Polis",
+        "votes": "42543"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "38196"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "2907"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "2623"
+    }],
+    "08103": [{
+        "candidate": "Jared Polis",
+        "votes": "548"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "2737"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "94"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "96"
+    }],
+    "08105": [{
+        "candidate": "Jared Polis",
+        "votes": "2459"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "3333"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "186"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "208"
+    }],
+    "08107": [{
+        "candidate": "Jared Polis",
+        "votes": "8863"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "5992"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "291"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "379"
+    }],
+    "08109": [{
+        "candidate": "Jared Polis",
+        "votes": "1868"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "1228"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "148"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "136"
+    }],
+    "08111": [{
+        "candidate": "Jared Polis",
+        "votes": "320"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "229"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "19"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "18"
+    }],
+    "08113": [{
+        "candidate": "Jared Polis",
+        "votes": "3448"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "1235"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "150"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "137"
+    }],
+    "08115": [{
+        "candidate": "Jared Polis",
+        "votes": "392"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "930"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "37"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "37"
+    }],
+    "08117": [{
+        "candidate": "Jared Polis",
+        "votes": "13212"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "6623"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "499"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "513"
+    }],
+    "08119": [{
+        "candidate": "Jared Polis",
+        "votes": "4451"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "10277"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "352"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "488"
+    }],
+    "08121": [{
+        "candidate": "Jared Polis",
+        "votes": "396"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "2111"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "45"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "55"
+    }],
+    "08123": [{
+        "candidate": "Jared Polis",
+        "votes": "52462"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "84082"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "4647"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "4457"
+    }],
+    "08125": [{
+        "candidate": "Jared Polis",
+        "votes": "875"
+    }, {
+        "candidate": "Walker Stapleton",
+        "votes": "3675"
+    }, {
+        "candidate": "Scott Helker",
+        "votes": "97"
+    }, {
+        "candidate": "Bill Hammons",
+        "votes": "64"
+    }]
+}]
