6ec352589dcb46444d9c8b43a891bead506c120a
githamm <khamm@denverpost.com>
2018-10-24 17:21:30 -0600

    Showing only Yuma on all paths

--- map/testdata.js
+++ map/testdata.js
@@ -1,7 +1,322 @@
 [
   {
-    "name": "Arapahoe",
-    "GEOID": "08005",
-    "votes": 235641
+    "countyname": "Adams",
+    "geoid": "08001",
+    "votes": 1
+  },
+  {
+    "countyname": "Alamosa",
+    "geoid": "08003",
+    "votes": 2
+  },
+  {
+    "countyname": "Arapahoe",
+    "geoid": "08005",
+    "votes": 2
+  },
+  {
+    "countyname": "Archuleta",
+    "geoid": "08007",
+    "votes": 3
+  },
+  {
+    "countyname": "Baca",
+    "geoid": "08009",
+    "votes": 3
+  },
+  {
+    "countyname": "Bent",
+    "geoid": "08011",
+    "votes": 4
+  },
+  {
+    "countyname": "Boulder",
+    "geoid": "08013",
+    "votes": 5
+  },
+  {
+    "countyname": "Broomfield",
+    "geoid": "08014",
+    "votes": 6
+  },
+  {
+    "countyname": "Chaffee",
+    "geoid": "08015",
+    "votes": 7
+  },
+  {
+    "countyname": "Cheyenne",
+    "geoid": "08017",
+    "votes": 9
+  },
+  {
+    "countyname": "Clear Creek",
+    "geoid": "08019",
+    "votes": 10
+  },
+  {
+    "countyname": "Conejos",
+    "geoid": "08021",
+    "votes": 12
+  },
+  {
+    "countyname": "Costilla",
+    "geoid": "08023",
+    "votes": 15
+  },
+  {
+    "countyname": "Crowley",
+    "geoid": "08025",
+    "votes": 18
+  },
+  {
+    "countyname": "Custer",
+    "geoid": "08027",
+    "votes": 21
+  },
+  {
+    "countyname": "Delta",
+    "geoid": "08029",
+    "votes": 26
+  },
+  {
+    "countyname": "Denver",
+    "geoid": "08031",
+    "votes": 31
+  },
+  {
+    "countyname": "Dolores",
+    "geoid": "08033",
+    "votes": 37
+  },
+  {
+    "countyname": "Douglas",
+    "geoid": "08035",
+    "votes": 44
+  },
+  {
+    "countyname": "Eagle",
+    "geoid": "08037",
+    "votes": 53
+  },
+  {
+    "countyname": "El Paso",
+    "geoid": "08041",
+    "votes": 64
+  },
+  {
+    "countyname": "Elbert",
+    "geoid": "08039",
+    "votes": 77
+  },
+  {
+    "countyname": "Fremont",
+    "geoid": "08043",
+    "votes": 92
+  },
+  {
+    "countyname": "Garfield",
+    "geoid": "08045",
+    "votes": 110
+  },
+  {
+    "countyname": "Gilpin",
+    "geoid": "08047",
+    "votes": 132
+  },
+  {
+    "countyname": "Grand",
+    "geoid": "08049",
+    "votes": 159
+  },
+  {
+    "countyname": "Gunnison",
+    "geoid": "08051",
+    "votes": 191
+  },
+  {
+    "countyname": "Hinsdale",
+    "geoid": "08053",
+    "votes": 229
+  },
+  {
+    "countyname": "Huerfano",
+    "geoid": "08055",
+    "votes": 275
+  },
+  {
+    "countyname": "Jackson",
+    "geoid": "08057",
+    "votes": 330
+  },
+  {
+    "countyname": "Jefferson",
+    "geoid": "08059",
+    "votes": 396
+  },
+  {
+    "countyname": "Kiowa",
+    "geoid": "08061",
+    "votes": 475
+  },
+  {
+    "countyname": "Kit Carson",
+    "geoid": "08063",
+    "votes": 570
+  },
+  {
+    "countyname": "La Plata",
+    "geoid": "08067",
+    "votes": 684
+  },
+  {
+    "countyname": "Lake",
+    "geoid": "08065",
+    "votes": 820
+  },
+  {
+    "countyname": "Larimer",
+    "geoid": "08069",
+    "votes": 984
+  },
+  {
+    "countyname": "Las Animas",
+    "geoid": "08071",
+    "votes": 1181
+  },
+  {
+    "countyname": "Lincoln",
+    "geoid": "08073",
+    "votes": 1418
+  },
+  {
+    "countyname": "Logan",
+    "geoid": "08075",
+    "votes": 1701
+  },
+  {
+    "countyname": "Mesa",
+    "geoid": "08077",
+    "votes": 2041
+  },
+  {
+    "countyname": "Mineral",
+    "geoid": "08079",
+    "votes": 2450
+  },
+  {
+    "countyname": "Moffat",
+    "geoid": "08081",
+    "votes": 2940
+  },
+  {
+    "countyname": "Montezuma",
+    "geoid": "08083",
+    "votes": 3527
+  },
+  {
+    "countyname": "Montrose",
+    "geoid": "08085",
+    "votes": 4233
+  },
+  {
+    "countyname": "Morgan",
+    "geoid": "08087",
+    "votes": 5080
+  },
+  {
+    "countyname": "Otero",
+    "geoid": "08089",
+    "votes": 6095
+  },
+  {
+    "countyname": "Ouray",
+    "geoid": "08091",
+    "votes": 7315
+  },
+  {
+    "countyname": "Park",
+    "geoid": "08093",
+    "votes": 8777
+  },
+  {
+    "countyname": "Phillips",
+    "geoid": "08095",
+    "votes": 10533
+  },
+  {
+    "countyname": "Pitkin",
+    "geoid": "08097",
+    "votes": 12639
+  },
+  {
+    "countyname": "Prowers",
+    "geoid": "08099",
+    "votes": 15167
+  },
+  {
+    "countyname": "Pueblo",
+    "geoid": "08101",
+    "votes": 18201
+  },
+  {
+    "countyname": "Rio Blanco",
+    "geoid": "08103",
+    "votes": 21841
+  },
+  {
+    "countyname": "Rio Grande",
+    "geoid": "08105",
+    "votes": 26209
+  },
+  {
+    "countyname": "Routt",
+    "geoid": "08107",
+    "votes": 31451
+  },
+  {
+    "countyname": "Saguache",
+    "geoid": "08109",
+    "votes": 37741
+  },
+  {
+    "countyname": "San Juan",
+    "geoid": "08111",
+    "votes": 45290
+  },
+  {
+    "countyname": "San Miguel",
+    "geoid": "08113",
+    "votes": 54348
+  },
+  {
+    "countyname": "Sedgwick",
+    "geoid": "08115",
+    "votes": 65217
+  },
+  {
+    "countyname": "Summit",
+    "geoid": "08117",
+    "votes": 78260
+  },
+  {
+    "countyname": "Teller",
+    "geoid": "08119",
+    "votes": 93913
+  },
+  {
+    "countyname": "Washington",
+    "geoid": "08121",
+    "votes": 112695
+  },
+  {
+    "countyname": "Weld",
+    "geoid": "08123",
+    "votes": 135234
+  },
+  {
+    "countyname": "Yuma",
+    "geoid": "08125",
+    "votes": 162281
   }
 ]
